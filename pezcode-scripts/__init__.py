5421f6cee2d461edfa579b25c3a32cee121aba92
pezcode <mail@rvrs.in>
2018-03-05 22:43:01 +0100

    add copy_custom_properties

--- pezcode-scripts/__init__.py
+++ pezcode-scripts/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "name": "pezcode script collection",
     'description': 'Miscellaneous scripts',
     "author": "pezcode",
-    "version": (0, 0, 0, 1),
+    "version": (0, 0, 0, 2),
     "blender": (2, 79, 0),
     'support': 'COMMUNITY',
     'tracker_url': 'https://github.com/pezcode/blender-scripts/issues',
@@ -15,9 +15,10 @@ if "bpy" in locals():
     import importlib
     importlib.reload(batch_rename)
     importlib.reload(bounds_mesh)
+    importlib.reload(copy_custom_properties)
     print("Reloaded multifiles")
 else:
-    from . import batch_rename, bounds_mesh
+    from . import batch_rename, bounds_mesh, copy_custom_properties
     print("Imported multifiles")
 
 import bpy
